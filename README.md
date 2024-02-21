358f2977e28e3d4c1b0c37d34fc831f9f7eca6f3
Wesley Couch <WesleyCouch@sprouts.com>
2024-02-21 11:43:09 -0700

    Removed IE11 support

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Lazy Load Images
 - Lazy load images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
   - `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
-- IE11 fallback included
 
 ## How to Use
 1) Add class `lazy` to the image
 2) Add `data-` to the src/srcset/sizes attributes
 
 ## Example
-`<img data-src="img_src" data-srcset="img_srcset" data-sizes="img_sizes" alt="img_alt" class="lazy">`
+`<img data-src="img_src" data-srcset="img_srcset" data-sizes="img_sizes" alt="Image" class="lazy">`
 
 
 
@@ -19,11 +18,10 @@
 # Lazy Loaded Background Images
 - Lazy load background images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
   - `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
-- IE11 fallback included
 
 ## How to Use
 1) Add class `lazy-bg` to the element
 2) Add `data-bg` with the image url as an attribute
 
 ## Example
-`<section class="c-html lazy-bg" data-bg="img_src"></section>`
+`<div class="lazy-bg" data-bg="img_src"></div>`
--- lazy-load.js
+++ lazy-load.js
@@ -1,72 +1,52 @@
 /*
 // Lazy loaded images
 */
-
-let lazyImgs = document.getElementsByClassName('lazy'),
-    lazyImgsObserverOptions = {
-      rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
-    };
+const lazyImgs = document.getElementsByClassName('lazy')
+const lazyImgsObserverOptions = {
+  rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
+};
 if (!!lazyImgs.length) {
-  let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-  if (!!isIE11) { // IE11
-    for (var i = 0; i < lazyImgs.length; i++) {
-      if (!!(lazyImgs[i] instanceof HTMLImageElement) && !!lazyImgs[i].classList.contains('lazy')) {
-        lazyImgs[i].dataset.src ? lazyImgs[i].src = lazyImgs[i].dataset.src : ''
-      }
-    }
-  } else { // Modern Browsers
-    let observer = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          let el = entry.target;
+  let observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        let el = entry.target;
+        observer.unobserve(el);
 
-          if (!!(el instanceof HTMLImageElement) && !!el.classList.contains('lazy')) {
-            el.dataset.src ? el.src = el.dataset.src : ''
-            el.dataset.srcset ? el.srcset = el.dataset.srcset : ''
-            el.dataset.sizes ? el.sizes = el.dataset.sizes : ''
-          }
-          observer.unobserve(el);
+        if (!!(el instanceof HTMLImageElement) && !!el.classList.contains('lazy')) {
+          el.dataset.src ? el.src = el.dataset.src : ''
+          el.dataset.srcset ? el.srcset = el.dataset.srcset : ''
+          el.dataset.sizes ? el.sizes = el.dataset.sizes : ''
         }
-      });
-    }, lazyImgsObserverOptions);
+      }
+    });
+  }, lazyImgsObserverOptions);
 
-    for (let img of lazyImgs) {
-      observer.observe(img);
-    }
+  for (let img of lazyImgs) {
+    observer.observe(img);
   }
 }
-
 
 
 /*
 // Lazy loaded background images
 */
-
-let lazyBgEls = document.getElementsByClassName('lazy-bg'),
-    lazyBgElsObserverOptions = {
-      rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
-    };
+const lazyBgEls = document.getElementsByClassName('lazy-bg')
+const lazyBgElsObserverOptions = {
+  rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
+};
 if (!!lazyBgEls.length) {
-  let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-  if (!!isIE11) { // IE11
-    for (var i = 0; i < lazyBgEls.length; i++) {
-      lazyBgEls[i].dataset.bg ? lazyBgEls[i].style.backgroundImage = `url("${lazyBgEls[i].dataset.bg}")` : ''
-    }
-  } else { // Modern Browsers
-    let observer = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          let el = entry.target;
+  let observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        let el = entry.target;
+        observer.unobserve(el);
 
-          el.dataset.bg ? el.style.backgroundImage = `url("${el.dataset.bg}")` : ''
-          
-          observer.unobserve(el);
-        }
-      });
-    }, lazyBgElsObserverOptions);
+        el.dataset.bg ? el.style.backgroundImage = `url("${el.dataset.bg}")` : ''
+      }
+    });
+  }, lazyBgElsObserverOptions);
 
-    for (let el of lazyBgEls) {
-      observer.observe(el);
-    }
+  for (let el of lazyBgEls) {
+    observer.observe(el);
   }
 }
