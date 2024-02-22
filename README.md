e5b9ee9afb2fc20efe8bdefdd0d31e25fcae5eb0
Wesley Couch <WesleyCouch@sprouts.com>
2024-02-22 13:51:06 -0700

    No longer need classes for element query

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Lazy Load Images
-- Lazy load images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
-  - `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
 
-## How to Use
-1) Add class `lazy` to the image
-2) Add `data-` to the src/srcset/sizes attributes
+Lazy load images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
+- `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
 
-## Example
-`<img data-src="img_src" data-srcset="img_srcset" data-sizes="img_sizes" alt="Image" class="lazy">`
+### How to create
+
+- Add `data-src` attribute with the image URL that will be changed out with the `src` attribute
+  - *Note: You can add `data-srcset` and `data-sizes` attributes as well, but `data-src` is required*
+
+### Example
+
+    <img data-src="img_src" data-srcset="img_srcset" data-sizes="img_sizes" alt="Image">
 
 
 
@@ -16,12 +19,14 @@
 
 
 # Lazy Loaded Background Images
-- Lazy load background images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
-  - `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
 
-## How to Use
-1) Add class `lazy-bg` to the element
-2) Add `data-bg` with the image url as an attribute
+Lazy load background images with JS utilizing the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
+- `rootMargin` set to `200px` in the script, but can be edited/removed. Also `threshold` can be utilized instead to fire observer when some/all of the image has scrolled into view
 
-## Example
-`<div class="lazy-bg" data-bg="img_src"></div>`
+### How to create
+
+- Add `data-bkg` attribute with the image URL that will be changed out with the "background-image" style
+
+### Background Image Example
+
+    <div data-bkg="img_src"></div>
--- lazy-load.js
+++ lazy-load.js
@@ -1,52 +1,50 @@
 /*
 // Lazy loaded images
 */
-const lazyImgs = document.getElementsByClassName('lazy')
+const lazyImgs = document.querySelectorAll('img[data-src]:not([data-src=""]')
 const lazyImgsObserverOptions = {
   rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
 };
 if (!!lazyImgs.length) {
-  let observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        let el = entry.target;
-        observer.unobserve(el);
+	let observer = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				let el = entry.target;
+				observer.unobserve(el);
 
-        if (!!(el instanceof HTMLImageElement) && !!el.classList.contains('lazy')) {
-          el.dataset.src ? el.src = el.dataset.src : ''
-          el.dataset.srcset ? el.srcset = el.dataset.srcset : ''
-          el.dataset.sizes ? el.sizes = el.dataset.sizes : ''
-        }
-      }
-    });
-  }, lazyImgsObserverOptions);
+				el.dataset.src ? el.src = el.dataset.src : '';
+				el.dataset.srcset ? el.srcset = el.dataset.srcset : '';
+				el.dataset.sizes ? el.sizes = el.dataset.sizes : '';
+			}
+		});
+	}, lazyImgsObserverOptions);
 
-  for (let img of lazyImgs) {
-    observer.observe(img);
-  }
+	for (let img of lazyImgs) {
+		observer.observe(img);
+	}
 }
 
 
 /*
 // Lazy loaded background images
 */
-const lazyBgEls = document.getElementsByClassName('lazy-bg')
-const lazyBgElsObserverOptions = {
+const lazyBkgEls = document.querySelectorAll('*[data-bkg]:not([data-bkg=""]')
+const lazyBkgElsObserverOptions = {
   rootMargin: '200px', //RootMargin declares where it will trigger the observer (optional). 200px means when you scroll to within 200px of the image
 };
-if (!!lazyBgEls.length) {
-  let observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        let el = entry.target;
-        observer.unobserve(el);
+if (!!lazyBkgEls.length) {
+	let observer = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				let el = entry.target;
+				observer.unobserve(el);
 
-        el.dataset.bg ? el.style.backgroundImage = `url("${el.dataset.bg}")` : ''
-      }
-    });
-  }, lazyBgElsObserverOptions);
+				el.dataset.bg ? el.style.backgroundImage = `url("${el.dataset.bg}")` : '';
+			}
+		});
+	}, lazyBkgElsObserverOptions);
 
-  for (let el of lazyBgEls) {
-    observer.observe(el);
-  }
+	for (let el of lazyBkgEls) {
+		observer.observe(el);
+	}
 }
